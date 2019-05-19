76b62222efa19205245241500163a9828289da34
sambaiz <godgourd@gmail.com>
2019-05-20 02:57:16 +0900

    create vpc

--- bin/aws-cdk-circleci-sample.ts
+++ bin/aws-cdk-circleci-sample.ts
@@ -2,6 +2,8 @@
 import 'source-map-support/register';
 import cdk = require('@aws-cdk/cdk');
 import { IAMCIStack } from '../lib/iam-ci-stack';
+import { VPCStack } from '../lib/vpc-stack';
 
 const app = new cdk.App();
 new IAMCIStack(app, 'IAMCIStack');
+new VPCStack(app, 'VPCStack');
---
+++ lib/vpc-stack.ts
@@ -0,0 +1,29 @@
+import cdk = require('@aws-cdk/cdk');
+import ec2 = require('@aws-cdk/aws-ec2')
+
+export class VPCStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+        [
+            { name: 'test-vpc', cidr: '172.32.0.0/24' }
+        ].forEach(v =>
+            new ec2.VpcNetwork(this, v.name, {
+                cidr: v.cidr,
+                maxAZs: 2,
+                subnetConfiguration: [
+                    {
+                        cidrMask: 26,
+                        subnetType: ec2.SubnetType.Public,
+                        name: `${v.name}-public`
+                    },
+                    {
+                        cidrMask: 26,
+                        subnetType: ec2.SubnetType.Private,
+                        name: `${v.name}-private`
+                    }
+                ]
+            })
+
+        )
+    }
+}
