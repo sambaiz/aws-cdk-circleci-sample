9ab17c0da8bdfc0a0b70bde79e4c2fff96cb7710
sambaiz <godgourd@gmail.com>
2019-06-08 20:22:00 +0900

    prettier

--- lib/some-app/some-app-stack.ts
+++ lib/some-app/some-app-stack.ts
@@ -2,27 +2,30 @@ import * as cdk from '@aws-cdk/cdk'
 import * as rds from '@aws-cdk/aws-rds'
 
 interface ImportValue {
-    dbSubnetIds: string[]
+  dbSubnetIds: string[]
 }
 
 export class SomeAppStack extends cdk.Stack {
-    protected deployEnv: string
-    private importValue: ImportValue
-    constructor(scope: cdk.Construct, id: string, importValue: ImportValue, props?: cdk.StackProps) {
-        super(scope, id, props);
-        this.deployEnv = this.node.getContext('env')
-        this.importValue = importValue
+  protected deployEnv: string
+  private importValue: ImportValue
+  constructor(
+    scope: cdk.Construct,
+    id: string,
+    importValue: ImportValue,
+    props?: cdk.StackProps
+  ) {
+    super(scope, id, props)
+    this.deployEnv = this.node.getContext('env')
+    this.importValue = importValue
 
-        this.dbSubnetGroup()
-    }
+    this.dbSubnetGroup()
+  }
 
-    private dbSubnetGroup() {
-        new rds.CfnDBSubnetGroup(this, 'DBSubnetGroup', {
-            dbSubnetGroupDescription: `${this.deployEnv}_subnet_group_for_some_app_db`,
-            subnetIds: this.importValue.dbSubnetIds,
-            dbSubnetGroupName: `${this.deployEnv}-some-app`
-        })
-    }
-
-
+  private dbSubnetGroup() {
+    new rds.CfnDBSubnetGroup(this, 'DBSubnetGroup', {
+      dbSubnetGroupDescription: `${this.deployEnv}_subnet_group_for_some_app_db`,
+      subnetIds: this.importValue.dbSubnetIds,
+      dbSubnetGroupName: `${this.deployEnv}-some-app`
+    })
+  }
 }
